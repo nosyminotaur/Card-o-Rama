dbec4fe011f526560a5773c7b372fddc8805ec39
Harsh Pathak <harshgonecrazy@outlook.com>
2019-02-25 10:33:08 +0530

    First Commit
    Default Values Controller Removed, added auth controller and most required nuget packages.
    SignalR already present in AspNetCore.App so no need to add it to project
    .NET Core Version 2.2

---
+++ CardGame-API/CardGame/CardGame/Controllers/AuthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CardGame.Controllers
+{
+    //Using APIController allows us to not validate Model state because it is done automatically
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        public AuthController() { }
+    }
+}
